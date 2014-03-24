a41aee84f8a352212b85d22466fdcfc7444caa8f
Steven Lu <tacticalazn@gmail.com>
2014-03-24 00:04:36 -0400

    Adding assets to base blade template.

---
+++ app/views/base.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+
+  <title></title>
+
+  <link href="{{ asset('assets/styles.css') }}" rel="stylesheet">
+
+  <script src="{{ asset('bower_components/jquery/dist/jquery.js') }}"></script>
+  <script src="{{ asset('bower_components/twitter/dist/js/bootstrap.js') }}"></script>
+  <script src="{{ asset('assets/scripts.js') }}"></script>
+</head>
+<body>
+
+  <div class="container">
+    @section('container')
+    @show
+  </div>
+
+</body>
+</html>
